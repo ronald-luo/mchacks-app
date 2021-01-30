997e15a07b72c720902efbf0049d05ce09529e91
ronald-luo <r.luo.180@gmail.com>
2021-01-30 15:46:26 -0500

    🤠 howdy🤠

---
+++ lib/dummy_data.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Mathematics',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Biology',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Chemistry',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Physics',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Computer Programming',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c6',
+    title: 'History',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c7',
+    title: 'French',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c8',
+    title: 'LSAT',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c9',
+    title: 'SAT',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c10',
+    title: 'MCAT',
+    color: Colors.teal,
+  ),
+];
